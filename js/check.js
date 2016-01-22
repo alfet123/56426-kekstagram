94c9e2ad87686920719210e149cacc0e489a62e7
Алексей Фетисов <alfet123@gmail.com>
2016-01-22 22:03:06 +0500

    Исправление
    Добавлена проверка параметров функции на соответствие массиву

--- js/check.js
+++ js/check.js
@@ -9,19 +9,21 @@ function getMessage(a, b) {
       result += 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов';
       break;
     case 'object':
-      if (typeof b == 'object') {
-        var square = 0;
-        for (i = 0; i < a.length; i++) {
-          square += a[i] * b[i];
+      if (Array.isArray(a)) {
+        if (Array.isArray(b)) {
+          var square = 0;
+          for (i = 0; i < a.length; i++) {
+            square += a[i] * b[i];
+          }
+          result += 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
         }
-        result += 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
-      }
-      else {
-        var sum = 0;
-        for (i = 0; i < a.length; i++) {
-          sum += a[i];
+        else {
+          var sum = 0;
+          for (i = 0; i < a.length; i++) {
+            sum += a[i];
+          }
+          result += 'Количество красных точек во всех строчках изображения: ' + sum;
         }
-        result += 'Количество красных точек во всех строчках изображения: ' + sum;
       }
   }
 
